3c7ca04d1f0bd9cb6f3b8b531a822d5af5b62fc5
jjj-ols123 <fuentes.289565@sjdelmonte.sti.edu.ph>
2025-03-16 17:32:09 +0800

    authentication

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,28 +1,33 @@
-
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class AuthService { 
-  final SupabaseClient _supabase = Supabase.instance.client; 
+class AuthService {
+  final SupabaseClient _supabase = Supabase.instance.client;
 
-  //sign-in 
-  Future<AuthResponse> signIn({required String email, required String password}) async {
-    return await _supabase.auth.signInWithPassword(email: email, password: password); 
+  //sign-in
+  Future<AuthResponse> signIn(
+      {required String email, required String password}) async {
+    return await _supabase.auth
+        .signInWithPassword(email: email, password: password);
   }
 
   //sign-up
-  Future<AuthResponse> signUp({required String email, required String password, Map<String, dynamic>? data,}) async {
-    return await _supabase.auth.signUp(email: email, password: password, data: data); 
+  Future<AuthResponse> signUp({
+    required String email,
+    required String password,
+    Map<String, dynamic>? data,
+  }) async {
+    return await _supabase.auth
+        .signUp(email: email, password: password, data: data);
   }
 
   //sign-out
 
   Future<void> signOut() async {
-    await _supabase.auth.signOut(); 
+    await _supabase.auth.signOut();
   }
 
   //anonymous sign-in
   Future<AuthResponse> signInAnonymously() async {
-    return await _supabase.auth.signInAnonymously(); 
+    return await _supabase.auth.signInAnonymously();
   }
-
 }
