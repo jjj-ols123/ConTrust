8ab78c9c96f0e647ecf03a9aa70b39f0722d2f6a
John Code <fuentes.289565@sjdelmonte.sti.edu.ph>
2025-09-11 12:51:11 +0800

    login modal not showing in website fixed (contractee)

--- contrust-main/Contractee/lib/services/cee_checkuser.dart
+++ contrust-main/Contractee/lib/services/cee_checkuser.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; 
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:contractee/pages/cee_login.dart'; 
 
@@ -16,15 +17,31 @@ class CheckUserLogin {
     if (session != null) {
       onAuthenticated();
     } else {
-      showModalBottomSheet(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-        ),
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: Colors.transparent,
-        builder: (modalContext) => LoginPage(modalContext: modalContext),
-      );
+      if (kIsWeb) {
+        showDialog(
+          context: context,
+          builder: (dialogContext) => Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: SizedBox(
+              width: 600, 
+              height: 1000, 
+              child: LoginPage(modalContext: dialogContext),
+            ),
+          ),
+        );
+      } else {
+        showModalBottomSheet(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          context: context,
+          isScrollControlled: true,
+          backgroundColor: Colors.transparent,
+          builder: (modalContext) => LoginPage(modalContext: modalContext),
+        );
+      }
     }
   }
 }
