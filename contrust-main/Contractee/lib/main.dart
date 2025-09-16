836601c9fc31f58163666cb711655e27fc392b97
John Code <fuentes.289565@sjdelmonte.sti.edu.ph>
2025-09-16 23:47:45 +0800

    hidden database key

--- contrust-main/Contractee/lib/main.dart
+++ contrust-main/Contractee/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:contractee/pages/cee_welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:backend/utils/supabase_config.dart';
 
 class AppScrollBehavior extends MaterialScrollBehavior {
   @override
@@ -14,13 +15,10 @@ class AppScrollBehavior extends MaterialScrollBehavior {
       };
 }
 
-final String bgScreen = "https://bgihfdqruamnjionhkeq.supabase.co/storage/v1/object/public/app//bgloginscreen.jpg";
-
 Future<void> main() async {
   await Supabase.initialize(
-    url: 'https://bgihfdqruamnjionhkeq.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnaWhmZHFydWFtbmppb25oa2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NzIyODksImV4cCI6MjA1NjQ0ODI4OX0.-GRaolUVu1hW6NUaEAwJuYJo8C2X5_1wZ-qB4a-9Txs',
-    
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
   );
 
   SharedPreferences prefs = await SharedPreferences.getInstance();
--- contrust-main/Contractor/lib/main.dart
+++ contrust-main/Contractor/lib/main.dart
@@ -4,15 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:backend/utils/supabase_config.dart';
 
-final url = 'https://bgihfdqruamnjionhkeq.supabase.co';
-final key =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnaWhmZHFydWFtbmppb25oa2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NzIyODksImV4cCI6MjA1NjQ0ODI4OX0.-GRaolUVu1hW6NUaEAwJuYJo8C2X5_1wZ-qB4a-9Txs';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: url, anonKey: key);
-
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
   runApp(const MyApp());
 }
 
