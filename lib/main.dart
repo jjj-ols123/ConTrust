330c5949ec76b30b149cea914ee635786aad2c3e
John Code <fuentes.289565@sjdelmonte.sti.edu.ph>
2025-09-17 08:15:20 +0800

    changed to configuraiton again. env not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
+import 'package:backend/utils/supabase_config.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'models/app_websitestart.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: ".env");
   
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
   );
 
   runApp(const MyApp());
