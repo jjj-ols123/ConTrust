d6d62a8bd39d90326361feea3783d0fdc0567283
John Code <fuentes.289565@sjdelmonte.sti.edu.ph>
2025-10-22 10:20:09 +0800

    show photos on deployed web

--- contrust-main/Contractor/lib/Screen/cor_registration.dart
+++ contrust-main/Contractor/lib/Screen/cor_registration.dart
@@ -38,7 +38,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/bgloginscreen.jpg'),
+            image: AssetImage('assets/images/bgloginscreen.jpg'),
             fit: BoxFit.cover,
           ),
         ),
@@ -218,7 +218,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       children: [
         Column(
           children: [
-            Image.asset('images/logo3.png', width: 80, height: 80),
+            Image.asset('assets/images/logo3.png', width: 80, height: 80),
             const SizedBox(height: 16),
             Text(
               'Join ConTrust',
