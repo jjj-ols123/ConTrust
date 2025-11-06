a093f5b6923bbd48c86c23528f857aecd38c282e
pau <laniogpaulorafael087@gmail.com>
2025-11-07 07:59:55 +0800

    dependencies

--- contrust-main/websitepoint/macos/Flutter/GeneratedPluginRegistrant.swift
+++ contrust-main/websitepoint/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import google_sign_in_ios
+import mobile_scanner
 import path_provider_foundation
 import printing
 import quill_native_bridge_macos
@@ -25,6 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
